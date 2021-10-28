4dc52234034d1c8c56882ddb08c6b10f82f2bf56
Samrat sasmal <opg66068@gmail.com>
2021-10-29 01:53:42 +0530

    teast 2.0

--- From-controler.php
+++ From-controler.php
@@ -5,15 +5,15 @@ $message=$_POST['massage'];
 
 
 
-$email_from='samratsasmal2021@gmail.com';
-$email_subject="new from submission";
-$email_body="user name: $name.\n".
+$email_from = 'samratsasmal2021@gmail.com';
+$email_subject = "new from submission";
+$email_body = "user name: $name.\n".
               "user email: $visitor_email.\n".
                 "user massage: $massage.\n";
 
-$to="samratsasmal2021@gmail.com";
-$headers="from: $email_from \r\n";
-$headers .="Reply-To: $visitor_email \r\n";
+$to = "samratsasmal2021@gmail.com";
+$headers = "from: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 header("location: index.html");
 ?>
